cb7b23f56a25e368c78462ca37df5a5530c66163
Michael M Santos <mmss240610@gmail.com>
2024-10-08 16:11:18 -0300

    propostas e visualizacao

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,9 +19,12 @@ class DatabaseSeeder extends Seeder
 
         User::query()->inRandomOrder()->limit(20)->get()
             ->each(
-                fn(User $u) =>
+                function (User $u) {
 
-                Project::factory()->create(['created_by' => $u->id])
+                    $project = Project::factory()->create(['created_by' => $u->id]);
+
+                    Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+                }
             );
     }
 }
