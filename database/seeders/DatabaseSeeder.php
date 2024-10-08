817acbde7dd89c601d885041293f68156b162cbb
Michael M Santos <mmss240610@gmail.com>
2024-10-08 11:27:57 -0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(20)->create();
+
+        User::query()->inRandomOrder()->limit(20)->get()
+            ->each(
+                fn(User $u) =>
+
+                Project::factory()->create(['created_by' => $u->id])
+            );
+    }
+}
