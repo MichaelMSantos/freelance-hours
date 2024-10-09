25230b4cd5824adbe742e14153d5e58f4ff5b051
Michael M Santos <mmss240610@gmail.com>
2024-10-09 16:42:49 -0300

    timer

--- app/Livewire/Projects/Timer.php
+++ app/Livewire/Projects/Timer.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\Projects;
 
+use App\Models\Project;
 use Livewire\Component;
 
 class Timer extends Component
 {
+
+    public Project $project;
+
+    public function timer() {
+
+    }
     public function render()
     {
-        return view('livewire.projects.timer');
+
+        $diff = now()->diff($this->project->ends_at);
+
+        return view('livewire.projects.timer', [
+            'days' => $diff->d,
+            'hours' => $diff->h,
+            'minutes' => $diff->m,
+            'seconds' => $diff->s,
+        ]);
     }
 }
